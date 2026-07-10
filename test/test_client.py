84912e5e68d75b5f1e0131ee96882c7664e99543
GIGI WANG <gigiwangs@outlook.com>
2026-07-10 12:50:58 +0800

    TEST: add test

--- test/test_client.py
+++ test/test_client.py
@@ -1,30 +1,35 @@
 # -*- coding: utf-8 -*-
 import json
 import threading
+import pytest
+from unittest.mock import patch 
 
 from dubbo_ssl.client import DubboClient
 from dubbo_ssl.client import ZkRegister
 from dubbo_ssl.codec.encoder import Object
-import logging
+from dubbo_ssl.common.exceptions import (RegisterException)
 
-logger = logging.getLogger('dubbo_ssl')
-logging.basicConfig(level=logging.INFO)
+class TestDubboClient:
+
+    def test_init_without_host_and_zk_raises(self):
+        with pytest.raises(RegisterException):
+            DubboClient(interface="com.example.Svc",group="default")
+    
+    def test_init_with_host(self):
+        client = DubboClient(interface="com.example.Svc",group="default",
+                             host="127.0.0.1:20880",verify=None)
+        assert client is not None
+
+    def test_call_auto_wraps_single_arg(self):
+        client = DubboClient(interface="com.example.Svc",group="default",
+                             host="127.0.0.1:20880",verify=None)
+        with patch("dubbo_ssl.client.connection_pool") as mock_pool:
+            mock_pool.get.return_value = "result"
+            client.call(method="process",args="single_arg")
+            call_args = mock_pool.get.call_args
+            request_param = call_args[0][1]
+            assert request_param["arguments"] == ["single_arg"]
 
 
-def pretty_print(value):
-    print(json.dumps(value, ensure_ascii=False, indent=4, sort_keys=True))
 
 
-zk_client = ZkRegister('')
-
-client = DubboClient('', zk_register=zk_client)
-
-java_obj = Object('')
-java_obj[''] = ''
-java_obj[''] = ''
-
-for i in range(100):
-    result = client.call('getMemberInfo', java_obj)
-    pretty_print(result)
-
-# threading.Event().wait()
